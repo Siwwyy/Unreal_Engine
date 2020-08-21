1d69462e419deaaadaaf45c8afa3f3fdb5a6773a
Damian Andrysiak <andrydamian@gmail.com>
2020-08-21 17:35:51 +0200

    up

--- Tutorials/Light_Door/Source/Light_Door/Light_Door.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Light_Door.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Light_Door, "Light_Door" );
- 
