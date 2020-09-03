a999ccf822ccf4a76efa3bd493b7df9014089156
Damian Andrysiak <andrydamian@gmail.com>
2020-09-03 16:46:03 +0200

    added music perception and damage dealing NPC and Player both

--- Tutorials/Light_Door/Intermediate/Build/Win64/Light_DoorEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,1 +0,0 @@
-#include "E:/UNREAL ENGINE/Tutorials/Light_Door/Intermediate/Build/Win64/Light_DoorEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- Tutorials/Light_Door/Source/Light_Door/Swing_Door.cpp
+++
@@ -1,7 +0,0 @@
-
-#include "Swing_Door.h"
-
-ASwing_Door::ASwing_Door()
-{
-	PrimaryActorTick.bCanEverTick = false;
-}
